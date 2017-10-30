78ed016b13449a05a2753df82d432e9dec67e177
paronne <paronne@servoy.com>
2017-10-30 18:13:23 +0200

    Add code sample in provider

--- svyWebStorageExample/forms/localStorageExampleProvider.js
+++ svyWebStorageExample/forms/localStorageExampleProvider.js
@@ -9,31 +9,44 @@ function getName() {
 }
 
 /**
-*
-* @return {String}
-*
-* @properties={typeid:24,uuid:"D1AD81E3-219B-4187-A5FC-5ACE6FD1597A"}
-*/
+ *
+ * @return {String}
+ *
+ * @properties={typeid:24,uuid:"D1AD81E3-219B-4187-A5FC-5ACE6FD1597A"}
+ */
 function getDescription() {
 	return 'Web Local Storage'
 }
 
 /**
-*
-* @return {RuntimeForm<AbstractMicroSample>}
-*
-* @properties={typeid:24,uuid:"E4F36A3B-91D7-46B6-B522-2E59FEB6B8FE"}
-*/
+ *
+ * @return {RuntimeForm<AbstractMicroSample>}
+ *
+ * @properties={typeid:24,uuid:"E4F36A3B-91D7-46B6-B522-2E59FEB6B8FE"}
+ */
 function getParent() {
 	return forms.ngServiceSamples;
 }
 
 /**
-*
-* @return {String}
-*
-* @properties={typeid:24,uuid:"F61907CB-0CD1-48D1-8DDF-C799FA44E8F5"}
-*/
+ *
+ * @return {String}
+ *
+ * @properties={typeid:24,uuid:"F61907CB-0CD1-48D1-8DDF-C799FA44E8F5"}
+ */
 function getIconStyleClass() {
 	return 'fa fa-database';
+}
+/**
+ * @public
+ * @return {Array<String>} code lines
+ * @SuppressWarnings(private) TODO suppress not working ?
+ * @properties={typeid:24,uuid:"7DDBCAC0-B45A-4BC4-95C2-4304F82D2BFC"}
+ */
+function getSampleCode() {
+	return printMethodCode(forms.localStorageSample.setItemSearchText)
+	.concat(printMethodCode(forms.localStorageSample.getItemSearchText))
+	.concat(printMethodCode(forms.localStorageSample.removeItemSearchText))
+	.concat(printMethodCode(forms.localStorageSample.clearAll));
+
 }
